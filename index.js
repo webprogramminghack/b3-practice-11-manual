85843a73fccdf47d00b0c66e4affbfa30f151afd
myusername <71491992+kyraongithub@users.noreply.github.com>
2025-04-08 15:10:36 +0700

    init commit class 11 - manual TS code

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+const sum = (a, b) => {
+  return a + b;
+};
+sum(3, '4');
+
+const PI = 3.14;
---
+++ index.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+sum(3, '4');
+
+const PI: number = 3.14;
